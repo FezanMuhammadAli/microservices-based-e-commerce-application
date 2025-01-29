805ddab3b7185c9c811497f7dc10c2c906f12b55
Fezan Muhammad Ali <129467917+FezanMuhammadAli@users.noreply.github.com>
2025-01-29 13:17:04 -0800

    Update app.py

--- product-service/app.py
+++ product-service/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 products = [
     {"id": 1, "name": "Laptop", "price": 1200},
     {"id": 2, "name": "Smartphone", "price": 800},
-    {"id": 3, "name": "Tablet", "price": 500},
+    {"id": 3, "name": "Tablet", "price": 1500},
 ]
 
 @app.route('/products', methods=['GET'])
